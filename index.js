fb0c1c1a15a1571c6134fdfc9f261dc42c1f1cdb
Marcio Sales <marcio@netpark.com.br>
2023-11-24 10:08:19 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+
+let app = require('express')();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+    }
+});
+const cors = require('cors');
+app.use(cors());
+
+let resps = ['Olá, estamos conectados', 'Excelentíssimo Dev, você está demonstrando o funcionamento do socket.io', 'Disponha']
+let currentResp = 0;
+
+io.on('connection', (socket) => {
+    //Token do usuário
+    console.log(socket.handshake.headers.authorization);
+
+    socket.on('disconnect', function(){
+      io.emit('usersActivity', {
+        event: 'chatLeft'
+      });
+    });
+    
+    socket.on('sendTheMessage', (message) => {
+      io.emit('message', {
+        msg: message.text,
+        createdAt: new Date(),
+        resp: resps[currentResp]
+      });    
+
+      currentResp++
+
+      if (currentResp === 3) {
+        currentResp = 0;
+      }
+    });
+  });
+
+let port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log(port + ' Server Started');
+});
